ac46f2c147bc0b99cbcd7ec24cc9abaa938bd1a6
george-git-dev <george_roots@hotmail.com>
2026-06-19 18:46:27 -0300

    Initialize spring boot project

---
+++ src/main/java/itau_balance_api/ItauBalanceApiApplication.java
@@ -0,0 +1,13 @@
+package itau_balance_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItauBalanceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItauBalanceApiApplication.class, args);
+	}
+
+}
