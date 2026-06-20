b6373a37c4192bf0c0d5386449a7b5c0279d8cc9
george-git-dev <george_roots@hotmail.com>
2026-06-20 17:04:31 -0300

    Implement basic SQS consumption

--- src/main/java/itau_balance_api/consumer/SqsConsumer.java
+++ src/main/java/itau_balance_api/consumer/SqsConsumer.java
@@ -1,24 +1,41 @@
 package itau_balance_api.consumer;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import itau_balance_api.service.AccountService;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.Message;
+import software.amazon.awssdk.services.sqs.model.ReceiveMessageRequest;
+
+import java.util.List;
 
 @Component
 public class SqsConsumer {
 
-    private final AccountService service;
-    private final ObjectMapper mapper;
+    private final SqsClient sqsClient;
+    private final String queueUrl = "http://localhost:4566/000000000000/transacoes-financeiras-processadas";
 
-    public SqsConsumer(AccountService service, ObjectMapper mapper) {
-        this.service = service;
-        this.mapper = mapper;
+    public SqsConsumer(SqsClient sqsClient) {
+        this.sqsClient = sqsClient;
     }
 
-    @Scheduled(fixedDelay = 5000)
+    @Scheduled(fixedDelay = 3000)
     public void consume() {
-        System.out.println("Consumer running...");
+
+        ReceiveMessageRequest request = ReceiveMessageRequest.builder()
+                .queueUrl(queueUrl)
+                .maxNumberOfMessages(10)
+                .build();
+
+        List<Message> messages = sqsClient.receiveMessage(request).messages();
+
+        if (!messages.isEmpty()) {
+            System.out.println("Received messages: " + messages.size());
+        }
+
+        for (Message message : messages) {
+            System.out.println(message.body());
+        }
+
     }
 
 }
